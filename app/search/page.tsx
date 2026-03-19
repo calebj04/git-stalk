82522ac6b6c3a2cf523ae5970f8b5f8e33db13c8
Caleb Jennings <calebbirtch.j@gmail.com>
2026-03-18 23:53:54 -0400

    clean api

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,4 @@
-import { getGitHubUser, getLastActiveCommit } from "@/lib/gitAPI";
+import { getUserInfo } from "@/lib/gitAPI";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,7 +11,7 @@ export default async function SearchUsername({
 
   let user;
   try {
-    user = await getGitHubUser(search);
+    user = await getUserInfo(search);
   } catch (err: unknown) {
     if (err instanceof Error) {
       return <div className="text-2xl">Error: {err.message}</div>;
@@ -20,36 +20,39 @@ export default async function SearchUsername({
     }
   }
 
-  const lastActive = await getLastActiveCommit(user.login);
-
   return (
     <div className="w-full flex pt-12">
       <div>
-      <div className="text-2xl flex flex-col bg-white p-3 rounded-2xl">
-        <div className="flex pb-2">
-          <div className="relative w-24 h-24">
-            <Link href={user.html_url} target="_blank">
-              <Image
-                src={user.avatar_url}
-                alt={user.login}
-                className="h-full w-full object-cover rounded-full"
-                fill
-              />
-            </Link>
-          </div>
-          <div className="ml-2 flex flex-col justify-center">
-            <div className="text-4xl text-black">
-              <Link href={user.html_url} target="_blank">
-                {user.name || user.login}
+        <div className="text-2xl flex flex-col bg-white p-3 rounded-2xl">
+          <div className="flex pb-2">
+            <div className="relative w-24 h-24">
+              <Link href={user.user.html_url} target="_blank">
+                <Image
+                  src={user.user.avatar_url}
+                  alt={user.user.login}
+                  className="h-full w-full object-cover rounded-full"
+                  fill
+                />
               </Link>
             </div>
-            <Link href={lastActive?.url || ""} target="_blank">
-              <div className="text-sm text-gray-500">Active {lastActive?.time}</div>
-            </Link>
+            <div className="ml-2 flex flex-col justify-center">
+              <div className="text-4xl text-black">
+                <Link href={user.user.html_url} target="_blank">
+                  {user.user.name || user.user.login}
+                </Link>
+              </div>
+              <Link
+                href={user.lastActive.url || user.user.html_url}
+                target="_blank"
+              >
+                <div className="text-sm text-gray-500">
+                  Active {user.lastActive.time}
+                </div>
+              </Link>
+            </div>
           </div>
+          <div className="bg-gray-500 w-full h-0.5 rounded-full"></div>
         </div>
-        <div className="bg-gray-500 w-full h-0.5 rounded-full"></div>
-      </div>
       </div>
       <div className="text-2xl flex-1 flex justify-center pl-3">
         Results for:
--- lib/gitAPI.ts
+++ lib/gitAPI.ts
@@ -13,56 +13,82 @@ type UserEvents = Awaited<
   ReturnType<Octokit["rest"]["activity"]["listPublicEventsForUser"]>
 >["data"];
 
-type LastActiveCommit = {
-  time: string;
-  url: string | null;
-} | null;
+type UserInfo = {
+  user: GitHubUser;
 
-export async function getGitHubUser(username: string): Promise<GitHubUser> {
-  try {
-    const res = await octokit.request(`GET /users/${username}`, {
-      username,
-      headers: {
-        "X-GitHub-Api-Version": "2026-03-10",
-      },
-    });
-    return res.data;
-  } catch (err) {
-    const error = err as RequestError;
-    if (error.status === 404) {
-      throw new Error(`User "${username}" not found.`);
-    } else if (error.status === 403) {
-      throw new Error("Rate limit exceeded. Try again later.");
-    } else {
-      throw new Error(`Unexpected error: ${error.message}`);
-    }
-  }
-}
+  stats: {
+    followers: number;
+    following: number;
+    repos: number;
+    stars: number;
+  };
 
-export async function getLastActiveCommit(
+  lastActive: {
+    time: string;
+    url: string | null;
+  };
+};
+
+export async function getUserInfo(
   username: string,
-): Promise<LastActiveCommit> {
+): Promise<UserInfo> {
   try {
-    const res = await octokit.request("GET /users/{username}/events/public", {
-      username,
-      per_page: 10,
-      headers: {
-        "X-GitHub-Api-Version": "2026-03-10",
-      },
-    });
+    const [userRes, eventsRes, starredRes] = await Promise.all([
+      octokit.request("GET /users/{username}", {
+        username,
+        headers: { "X-GitHub-Api-Version": "2026-03-10" },
+      }),
 
-    const events: UserEvents = res.data;
+      octokit.request("GET /users/{username}/events/public", {
+        username,
+        per_page: 10,
+        headers: { "X-GitHub-Api-Version": "2026-03-10" },
+      }),
 
-    // Find first PushEvent (contains commits)
-    const pushEvent = events.find((event) => event.type === "PushEvent");
+      octokit.request("GET /users/{username}/starred", {
+        username,
+        per_page: 1,
+        headers: { "X-GitHub-Api-Version": "2026-03-10" },
+      }),
+    ]);
 
-    const lastActive = pushEvent?.created_at
-      ? timeAgo(pushEvent?.created_at)
+    const user = userRes.data;
+    const events: UserEvents = eventsRes.data;
+
+    const pushEvent = events.find((e) => e.type === "PushEvent");
+
+    const lastActiveTime = pushEvent?.created_at
+      ? timeAgo(pushEvent.created_at)
       : "No recent activity";
 
+    const repoUrl = pushEvent?.repo?.name
+      ? `https://github.com/${pushEvent.repo.name}`
+      : null;
+
+    let starredCount = 0;
+    const linkHeader = starredRes.headers.link;
+
+    if (linkHeader) {
+      const match = linkHeader.match(/&page=(\d+)>; rel="last"/);
+      if (match) starredCount = parseInt(match[1], 10);
+    } else {
+      starredCount = starredRes.data.length;
+    }
+
     return {
-      time: lastActive,
-      url: pushEvent?.repo.url ?? null,
+      user,
+
+      stats: {
+        followers: user.followers,
+        following: user.following,
+        repos: user.public_repos,
+        stars: starredCount,
+      },
+
+      lastActive: {
+        time: lastActiveTime,
+        url: repoUrl,
+      },
     };
   } catch (err) {
     const error = err as RequestError;
