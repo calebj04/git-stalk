e5372a24203864fc2b8b835fbc9d256b5a788658
Caleb Jennings <calebbirtch.j@gmail.com>
2026-03-19 00:22:13 -0400

    list stats

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -20,10 +20,33 @@ export default async function SearchUsername({
     }
   }
 
+  const stats = [
+    {
+      label: "Followers",
+      value: user.stats.followers,
+      url: `https://github.com/${user.user.login}?tab=followers`,
+    },
+    {
+      label: "Following",
+      value: user.stats.following,
+      url: `https://github.com/${user.user.login}?tab=following`,
+    },
+    {
+      label: "Repositories",
+      value: user.stats.repos,
+      url: `https://github.com/${user.user.login}?tab=repositories`,
+    },
+    {
+      label: "Stars",
+      value: user.stats.stars,
+      url: `https://github.com/${user.user.login}?tab=stars`,
+    },
+  ];
+
   return (
-    <div className="w-full flex pt-12">
+    <div className="w-full flex pt-12 mb-6">
       <div>
-        <div className="text-2xl flex flex-col bg-white p-3 rounded-2xl">
+        <div className="text-2xl flex flex-col bg-white p-6 rounded-2xl min-w-96">
           <div className="flex pb-2">
             <div className="relative w-24 h-24">
               <Link href={user.user.html_url} target="_blank">
@@ -51,12 +74,23 @@ export default async function SearchUsername({
               </Link>
             </div>
           </div>
-          <div className="bg-gray-500 w-full h-0.5 rounded-full"></div>
+          <div className="bg-gray-500 w-full h-0.5 mb-3 rounded-full"></div>
+          <div className="grid grid-cols-2 gap-3">
+            {stats.map((stat, i) => {
+              return (
+                <Link key={i} href={stat.url} target="_blank">
+                  <div className="bg-gray-50 aspect-square rounded-xl text-black flex flex-col items-center justify-center">
+                    <p>{stat.label}:</p>
+                    <p>{stat.value}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div className="text-2xl flex-1 flex justify-center pl-3">
         Results for:
-        {user.login}
       </div>
     </div>
   );
