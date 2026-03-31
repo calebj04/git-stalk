85c57ae43ec20e5f14b6ad2963c81c1a61d83d1a
Caleb Jennings <calebbirtch.j@gmail.com>
2026-03-31 10:33:35 -0400

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Cutive_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
@@ -36,6 +37,7 @@ export default function RootLayout({
         >
           {children}
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
